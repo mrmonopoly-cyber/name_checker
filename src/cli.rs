ec44589d8c2905785f0ac7c5f46c1d145b8fa120
Alberto Damo <alberto.damo@gmail.com>
2026-04-28 15:06:27 +0200

    feat(cli): added cli module

---
+++ src/cli.rs
@@ -0,0 +1,42 @@
+use clap::Parser;
+
+pub enum TestType{
+    Lexical,
+    PrimaDeclinazione,
+}
+
+pub struct ProgInput{
+    pub db_file: &'static str, 
+    pub test_type: TestType,
+}
+
+impl Default for ProgInput{
+    fn default() -> Self {
+        Self { db_file: "db_file.txt", test_type: TestType::Lexical}
+    }
+}
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    test_type: Option<String>,
+}
+
+pub fn parse_cli_args() -> ProgInput{
+    let args = Args::parse();
+    let mut res = ProgInput::default();
+
+    if let Some(s) = args.test_type{
+        match s.as_str() {
+            "names"  => res.test_type = TestType::Lexical,
+            "I dec" => res.test_type = TestType::PrimaDeclinazione,
+            _ => res.test_type = TestType::Lexical,
+        }
+    }
+
+
+    res
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,24 @@
 mod db;
 mod declinazione;
 mod exercise;
+mod cli;
 
-use clap::Parser;
 use std::process::exit;
 
 use db::DB;
 use self::declinazione::PRIMA_DECLINAZIONE;
 use self::exercise::Exercise;
 
-enum TestType{
-    Lexical,
-    PrimaDeclinazione,
-}
-
-struct ProgInput{
-    pub db_file: &'static str, 
-    pub test_type: TestType,
-}
-
-impl Default for ProgInput{
-    fn default() -> Self {
-        Self { db_file: "db_file.txt", test_type: TestType::Lexical}
-    }
-}
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    test_type: Option<String>,
-}
-
 fn main() {
-    let args = Args::parse();
-    let mut input = ProgInput::default();
-
-    if let Some(s) = args.test_type{
-        match s.as_str() {
-            "names"  => input.test_type = TestType::Lexical,
-            "I dec" => input.test_type = TestType::PrimaDeclinazione,
-            _ => input.test_type = TestType::Lexical,
-        }
-    }
+    let input = cli::parse_cli_args();
 
     match input.test_type{
-        TestType::Lexical => create_and_run_lex_exercise(&input),
-        TestType::PrimaDeclinazione => PRIMA_DECLINAZIONE.run_exercise(),
+        cli::TestType::Lexical => create_and_run_lex_exercise(&input),
+        cli::TestType::PrimaDeclinazione => PRIMA_DECLINAZIONE.run_exercise(),
     }
 }
 
-fn create_and_run_lex_exercise(input: &ProgInput)
+fn create_and_run_lex_exercise(input: &cli::ProgInput)
 {
     let db = match DB::new(::std::path::Path::new(input.db_file))
     {
