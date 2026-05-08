c8518bf277958e800c21cd2cd1faa49bc87a52ae
Alberto Damo <alberto.damo@gmail.com>
2026-05-09 00:14:44 +0200

    better user interaction, added ctrlc signal handler

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,14 @@ mod common;
 
 use std::io::Write;
 use std::process::exit;
-use self::db::DB;
-use self::exercise::ExeRes;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::time::Duration;
+
+use db::DB;
+use exercise::ExeRes;
+
+static RUNNING: AtomicBool = AtomicBool::new(true);
 
 fn main() {
     let mut user_input = String::new();
@@ -21,30 +27,69 @@ fn main() {
     }
     exercise.add_db(&db);
 
+    let (tx,rx) : (Sender<String>, Receiver<String>) = mpsc::channel();
+
+    ::std::thread::spawn(move ||{
+        let mut user_input = String::new();
+
+        loop{
+            user_input.clear();
+
+            if let Err(e) = get_user_input(&mut user_input){
+                println!("error reading user input {e}");
+                break;
+            }
+
+            if let Err(e) = tx.send(user_input.clone()){
+                println!("error sending user input {e}");
+                continue;
+            }
+        };
+    });
+
+    ctrlc::set_handler(move ||{
+        RUNNING.store(false, Ordering::Relaxed);
+    }).expect("error setting Ctrl-C handler");
+
     if exercise.num_exercise() != 0
     {
-        loop{
+        while RUNNING.load(Ordering::Relaxed){
             exercise.question(&mut user_input);
             print!("{}",user_input);
             if let Err(e) = ::std::io::stdout().flush(){
                 println!("error flush stdout: {e}");
             }
 
-            if let Err(e) = get_user_input(&mut user_input){
-                println!("error reading user input {e}");
-                continue;
-            }
+            loop{
+                match rx.recv_timeout(Duration::from_millis(100)){
+                    Ok(user_input) => {
+                        if user_input != exercise::QUIT_COMMAND {
+                            match exercise.answer(&user_input) {
+                                true => score.success(),
+                                false => score.fail(),
+                            }
+                        }
+                        else{
+                            RUNNING.store(false, Ordering::Relaxed);
+                        }
 
-            if user_input == exercise::QUIT_COMMAND {
-                break;
-            }
-
-            match exercise.answer(&user_input) {
-                true => score.success(),
-                false => score.fail(),
+                        break;
+                    },
+                    Err(mpsc::RecvTimeoutError::Disconnected) =>{
+                        println!("Disconnected");
+                        break;
+                    },
+                    Err(mpsc::RecvTimeoutError::Timeout) => {
+                        if !RUNNING.load(Ordering::Relaxed) {
+                            println!();
+                            break;
+                        }
+                    },
+                }
             }
         }
         println!("{}", score);
+        let _ =::std::io::stdout().flush();
     }
 
 }
